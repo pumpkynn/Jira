1a67a2505c16ee1592303561920c4d10f5339b78
pumpkynn <19198402685@163.com>
2025-10-03 23:16:11 +0800

    style: 用row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,33 +2,30 @@ import React from "react"
 import { ProjectListScreen } from "./screens/project-list"
 import { useAuth } from "./context/auth_context"
 import styled from "@emotion/styled"
+import { row } from "./components/lib"
 
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
     return <Container>
-        <Header>
-             <HeaderLeft>
-              <h3>logo</h3>
-              <h3>项目</h3>
-              <h3>用户</h3>
+        <Header between={true}>
+             <HeaderLeft gap={true}>
+              <h2>logo</h2>
+              <h2>项目</h2>
+              <h2>用户</h2>
             </HeaderLeft>
             <HeaderRight>
               RIGHT  <button onClick={logout}>退出</button>
             </HeaderRight>
           
         </Header>   
-        <Nav>
-            nav
-        </Nav>
+       
         <Main>
             <ProjectListScreen />
         </Main>  
         <Footer>
             footer
         </Footer>
-        <Aside>
-            aside
-        </Aside>
+       
     </Container>
 }
 
@@ -38,21 +35,17 @@ grid-template-rows: 6rem 1fr 6rem;
 grid-template-columns: 20rem 1fr 20rem;
 grid-template-areas: 
     "header header header"
-    "nav main aside"
+    "main main main"
     "footer footer footer";
   height: 100vh;
-  grid-gap: 5rem;
+ 
 `
-const Header = styled.header`
+const Header = styled(row)`
 grid-area: header;
 background-color: grey;
-display: flex;
-flex-direction:row;
-align-items: center;
-justify-content: space-between;
-`
-const HeaderLeft = styled.div`
 
+`
+const HeaderLeft = styled(row)`
 display: flex;
 flex-direction: row;
 align-items: center;
@@ -67,10 +60,6 @@ grid-area: main;
 const Footer = styled.footer`
 grid-area: footer;
 `
-const Aside = styled.aside`
-grid-area: aside;
 
-`
-const Nav = styled.nav`
-grid-area: nav;
-`
+
+
